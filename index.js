4eb017f98d59d3d3f73e4f8a667286d3c62855d4
Alize Lee <alizebreezes@gmail.com>
2021-01-18 17:30:10 +0100

    mongoDB and mongoose installed and configurations are done

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const { mongoURI } = require("./config/keys");
+const keys = require("./config/keys");
 require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
