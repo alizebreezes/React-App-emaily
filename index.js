683e15231d972508af8800e50e1ba2d5b124044e
Alize Lee <alizebreezes@gmail.com>
2021-01-21 18:34:08 +0100

    Authentication done, cookie-session has set

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const { mongoURI } = require("./config/keys");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
+require("./models/User");
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-// express tells to Node.js "I want to listen to port 5000"
