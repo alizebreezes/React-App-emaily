1a574fd2c26c6188ae47377d672385cd2df2a675
Alize Lee <alizebreezes@gmail.com>
2021-02-04 15:54:55 +0100

    header component condition depending on auth cases

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,26 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
@@ -9,12 +29,7 @@ class Header extends Component {
             Emaily
           </a>
           <ul id="nav-mobile" className="right">
-            <li>
-              <a href="/surveys">Dashboard</a>
-            </li>
-            <li>
-              <a href="/auth/google">Login With Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -22,4 +37,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
