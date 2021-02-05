bee9a0bcf23f0388610a29a02175f5ae26534882
Alize Lee <alizebreezes@gmail.com>
2021-02-05 18:01:27 +0100

    add description about payment info

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,10 +5,15 @@ class Payments extends Component {
   render() {
     return (
       <StripeCheckout
+        // customized text
+        name="Emaily"
+        description="5 $ for 5 email-survey credits"
         amount={500}
         token={(token) => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button className="btn">Add Credits</button>
+      </StripeCheckout>
     );
   }
 }
