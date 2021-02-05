6b327c0c0b9ac5d6524f8d937cd0ea88200e80c3
Alize Lee <alizebreezes@gmail.com>
2021-02-05 18:16:34 +0100

    front end side payment set up done. Posting token from Stripe

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import StripeCheckout from "react-stripe-checkout";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 class Payments extends Component {
   render() {
@@ -9,7 +11,7 @@ class Payments extends Component {
         name="Emaily"
         description="5 $ for 5 email-survey credits"
         amount={500}
-        token={(token) => console.log(token)}
+        token={(token) => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
       >
         <button className="btn">Add Credits</button>
@@ -18,4 +20,4 @@ class Payments extends Component {
   }
 }
 
-export default Payments;
+export default connect(null, actions)(Payments);
