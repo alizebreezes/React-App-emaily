b944c8b64b38e32bda635dd11503f65b43a94339
Alize Lee <alizebreezes@gmail.com>
2021-02-06 22:51:43 +0100

    show credits in header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -19,7 +19,8 @@ class Header extends Component {
           <li key="1">
             <Payments />
           </li>,
-          <li key="2">
+          <li key="2">Credits: {this.props.auth.credits}</li>,
+          <li key="3">
             <a href="/api/logout">Logout</a>
           </li>,
         ];
