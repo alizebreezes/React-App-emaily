58c85ca326b474abafa303587c97671982a4553e
Alize Lee <alizebreezes@gmail.com>
2021-02-06 22:32:18 +0100

    add middlewares to authenticate users for payment and credits

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -1,7 +1,6 @@
 modeule.exports = (req, res, next) => {
   if (!req.user) {
-    return res.status(401).send({ error: "You have to log in for this task!" });
-
+    return res.status(401).send({ error: "You have to log in first!" });
     next();
   }
 };
