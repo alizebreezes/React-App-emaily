a0d6e0514353235fffd34856330870454f851784
Alize Lee <alizebreezes@gmail.com>
2021-02-06 20:59:33 +0100

    fixing 504 error

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,4 +1,5 @@
 import { FETCH_USER } from "../actions/types";
+
 export default function (state = null, action) {
   switch (action.type) {
     case FETCH_USER:
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -18,8 +18,9 @@ module.exports = (app) => {
 
   app.get("/api/logout", (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect("/");
   });
+
   app.get("/api/current_user", (req, res) => {
     res.send(req.user);
   });
