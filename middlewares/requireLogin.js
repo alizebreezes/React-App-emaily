06c785c5ae5ebbc496c88b35c67309f377778999
Alize Lee <alizebreezes@gmail.com>
2021-02-06 22:25:23 +0100

    update

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -1,6 +1,6 @@
 modeule.exports = (req, res, next) => {
   if (!req.user) {
-    return res.status(401).send({ error: "You have to log in!" });
+    return res.status(401).send({ error: "You have to log in for this task!" });
 
     next();
   }
