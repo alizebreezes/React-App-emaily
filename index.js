47b6f0d5fa62288fb2b9ee7491e4a32a3971921d
Alize Lee <alizebreezes@gmail.com>
2021-02-08 16:23:55 +0100

    fixing deployment error

--- index.js
+++ index.js
@@ -24,11 +24,10 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
-// what to consider before deploying client side on heroku?
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets
   // like our main.js file, or main.css file!
-  app.get(express.static("/client/build"));
+  app.use(express.static("client/build"));
 
   // Express will serve up the index.html file
   // if it doesn't recognize the route
