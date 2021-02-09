d4599caf81dc0e7207780b28da3daabdf08e312e
Alize Lee <alizebreezes@gmail.com>
2021-02-09 17:03:49 +0100

    survey instance is set.

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -5,5 +5,19 @@ const requireCredits = require("../middlewares/requireCredits");
 const Survey = mongoose.model("surveys");
 
 module.exports = (app) => {
-  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {});
+  // Create new a survey and send out a big email
+  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients
+        .split(",")
+        .map((email) => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now(),
+    });
+  });
 };
