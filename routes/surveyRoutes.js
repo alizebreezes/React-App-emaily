617cf158da45eb8b0b11768a964fb77570a6d476
Alize Lee <alizebreezes@gmail.com>
2021-02-09 15:19:49 +0100

    survey route, credit middleware are created

---
+++ routes/surveyRoutes.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
+const requireCredits = require("../middlewares/requireCredits");
+
+const Survey = mongoose.model("surveys");
+
+module.exports = (app) => {
+  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {});
+};
