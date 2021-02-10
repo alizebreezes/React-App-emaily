604f23a025b69a9574a9610ae6a308808852447e
Alize Lee <alizebreezes@gmail.com>
2021-02-10 13:17:05 +0100

    adding helpers in Mailer from sendgrid

--- services/Mailer.js
+++ services/Mailer.js
@@ -11,6 +11,33 @@ class Mailer extends helper.Mail {
     this.subject = subject;
     this.body = new helper.Content("text/html", content);
     this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
+  }
+
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients() {
+    const personalize = new helper.Personalization();
+
+    this.recipients.forEach((recipent) => {
+      personalize.addTo(recipent);
+    });
+    this.addPersonalization(personalize);
   }
 }
 
