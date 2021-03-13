71d97bda76babfa26f78a12939dc17a239f9c62e
Alize Lee <alizebreezes@gmail.com>
2021-03-13 16:58:37 +0100

    client side is setup

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
+import { FETCH_SURVEYS } from "./types";
 
 export const fetchUser = () => async (dispatch) => {
   const res = await axios.get("/api/current_user");
@@ -17,4 +18,9 @@ export const submitSurvey = (values, history) => async (dispatch) => {
   const res = await axios.post("/api/surveys", values);
   history.push("/surveys");
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSurveys = () => async (dispatch) => {
+  const res = await axios.get("/api/surveys");
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
